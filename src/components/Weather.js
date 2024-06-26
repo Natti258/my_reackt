adb4b7af5f73e51c3a16efb09d83036e9e4161eb
Natti258 <lilao7486@gmail.com>
2024-06-26 16:48:59 +0300

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-
 function Weather({ data }) {
   const { name, main, weather, wind } = data;
   const currentDate = new Date();
@@ -30,36 +28,36 @@ function Weather({ data }) {
 }
 
 function getWeatherIcon(weatherCode) {
-    switch (weatherCode) {
-      case "01d":
-        return "https://s9.gifyu.com/images/SUvPm.gif"; // Солнечно
-      case "01n":
-        return "https://s9.gifyu.com/images/SUve6.gif"; // Яркая луна
-      case "02d":
-      case "02n":
-      case "03d":
-      case "03n":
-      case "04d":
-      case "04n":
-        return "https://s9.gifyu.com/images/SUvP7.gif"; // Облачно
-      case "09d":
-      case "09n":
-        return "https://s9.gifyu.com/images/SUvPd.gif"; // Дождь
-      case "10d":
-      case "10n":
-        return "https://s9.gifyu.com/images/SUvPQ.gif"; // Ливень
-      case "11d":
-      case "11n":
-        return "https://s9.gifyu.com/images/SUvPg.gif"; // Гроза
-      case "13d":
-      case "13n":
-        return "https://s9.gifyu.com/images/SUvP5.gif"; // Снег
-      case "50d":
-      case "50n":
-        return "https://s9.gifyu.com/images/SUvX2.gif"; // Туман
-      default:
-        return "https://s9.gifyu.com/images/SUvPm.gif"; // Неизвестно
-    }
+  switch (weatherCode) {
+    case "01d":
+      return "https://s9.gifyu.com/images/SUvPm.gif"; // Солнечно
+    case "01n":
+      return "https://s9.gifyu.com/images/SUve6.gif"; // Яркая луна
+    case "02d":
+    case "02n":
+    case "03d":
+    case "03n":
+    case "04d":
+    case "04n":
+      return "https://s9.gifyu.com/images/SUvP7.gif"; // Облачно
+    case "09d":
+    case "09n":
+      return "https://s9.gifyu.com/images/SUvPd.gif"; // Дождь
+    case "10d":
+    case "10n":
+      return "https://s9.gifyu.com/images/SUvPQ.gif"; // Ливень
+    case "11d":
+    case "11n":
+      return "https://s9.gifyu.com/images/SUvPg.gif"; // Гроза
+    case "13d":
+    case "13n":
+      return "https://s9.gifyu.com/images/SUvP5.gif"; // Снег
+    case "50d":
+    case "50n":
+      return "https://s9.gifyu.com/images/SUvX2.gif"; // Туман
+    default:
+      return "https://s9.gifyu.com/images/SUvPm.gif"; // Неизвестно
   }
-  
-  export default Weather;
+}
+
+export default Weather;
